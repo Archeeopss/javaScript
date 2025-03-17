05899acd2919ffc0290969fd6db3e4ca390387f9
Afrin <afrin091205@gmail.com>
2025-03-17 15:55:45 +0000

    git commit -m 'message'

---
+++ 02/05 objects.js
@@ -0,0 +1,45 @@
+/** Destructuring an object */
+
+const Player1 = {
+    name: "Oikawa Torru",
+    position: "Setter",
+    Team: "Seijoh"
+}
+
+// player1.Team
+
+const {Team} = Player1
+console.log(Team);
+
+const {position: pos} = Player1
+console.log(pos);
+
+/* const navbar = () => {
+
+}
+ navbar(company = a) //destructuring an object
+
+ //destructuring in react
+const navbar = ({company}) => {
+
+}
+*/
+
+/**API */
+//json structure:- {object format}
+/*
+{
+    "name": "Komi Haruki",
+    "position": "libero"
+    "Team": "Fukurodani"
+}
+*/
+
+//json structure [array format]
+/* 
+[
+    {Object1},
+    {Object2},
+    {Object3}....
+]
+*/

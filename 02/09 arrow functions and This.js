514b59fb072a661edeabe7d344059e22705f414e
Afrin <afrin091205@gmail.com>
2025-03-19 12:11:12 +0000

    git commit -m 'message'

--- 02/09 arrow functions and This.js
+++ 02/09 arrow functions and This.js
@@ -20,9 +20,43 @@ however, when this is run in browser console, this gives out windows as the obje
 
 /** ARROW FUNCTION */
 
-function any(){
+function any1(){
     let username = "Oscar"
     console.log(this.username);
 }
-any()
+any1()
 
+const any2 = function(){
+    let username = "Oscar"
+    console.log(this.username);
+}
+any2()
+
+const any3 = () => {
+    let username = "Oscar"
+    console.log(this);
+    console.log(this.username);
+}
+any3()
+
+//basic syntax of an arrow function
+
+const add = (num1, num2) => {
+    return num1 + num2; //explicit return
+}
+console.log(add(5, 8));
+
+//implicit return arrow function
+const sub = (num1, num2) =>  num1 - num2
+console.log(sub(24, 6));
+
+const mul = (num1, num2) =>  (num1 * num2)
+console.log(mul(4, 6));
+
+const uname = (num1) => ({un: "Chinen Miya"}) //returning an object
+console.log(uname(5));
+
+const arr = [2, 3, 5, 6, 8, 4, 7]
+arr.forEach(function() {})
+arr.forEach(() => {})
+arr.forEach(() => ())

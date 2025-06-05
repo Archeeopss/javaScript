cc3f387eb2d8589c60a65cd61d508923e13be3e4
Afrin <afrin091205@gmail.com>
2025-06-06 04:12:49 +0530

    Update 06 for each loop.js

--- 04 iterations/06 for each loop.js
+++ 04 iterations/06 for each loop.js
@@ -1,3 +1,15 @@
+//The forEach loop in JavaScript is a method used with arrays to execute a provided function once for each array element.
+/*SYNTAX:-
+array.forEach(function(element, index, array) {
+  code to execute
+});
+
+with arrow function:
+array.forEach((element, index, array) => {
+  // code to execute
+});
+*/
+
 let setters = ["Oikawa Torru", "Miya Atsumu", "Kozume Kenma", "Kageyama Tabio", "Akaashi Keiji", "Sugawara Koshi"]
 
 //callback function : recursive function
